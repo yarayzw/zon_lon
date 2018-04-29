a0da39e28e4ac85ab556283427cf9b354543cd84
USER-20180306TG\Administrator <2278757482@qq.com>
2018-04-29 18:25:02 +0800

    心跳包数据接收成功。

--- curl_url.php
+++ curl_url.php
@@ -14,32 +14,61 @@
         curl_setopt ( $curl, CURLOPT_CONNECTTIMEOUT, 0 );
         curl_setopt ( $curl, CURLOPT_CONNECTTIMEOUT_MS, 0 );
         curl_setopt ( $curl, CURLOPT_POST, 1 );  
-        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 );  
-        curl_setopt ( $curl, CURLOPT_TIMEOUT, 0);
+        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 ); 
+        curl_setopt ( $curl, CURLOPT_TIMEOUT, 0);  
         curl_setopt ( $curl, CURLOPT_SSL_VERIFYHOST, false);  
-        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);  
+        // curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);  
         curl_setopt ( $curl, CURLOPT_HEADER, true);  
                   
         $response = curl_exec($curl);
         $response = false;
         if($response === false)  
-        {  
-            if(curl_errno($curl) == CURLE_OPERATION_TIMEDOUT) curl_url();//超时处理操作
+        {
+            // var_dump('error---' . curl_errno($curl));
+            // //CURLE_OPERATION_TIMEDOUT == 28
+            // if(in_array(curl_errno($curl), [0, 28])) 
+            curl_url_($url);//超时处理操作
+            // if(curl_errno($curl) == CURLE_OPERATION_TIMEDOUT) curl_url();//超时处理操作
+        }
+    }
+
+    function curl_url_($url = '')
+    {
+        $curl = curl_init($url);
+        
+        curl_setopt ( $curl, CURLOPT_CONNECTTIMEOUT, 0 );
+        curl_setopt ( $curl, CURLOPT_CONNECTTIMEOUT_MS, 0 );
+        curl_setopt ( $curl, CURLOPT_POST, 1 );  
+        curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, 1 ); 
+        curl_setopt ( $curl, CURLOPT_TIMEOUT, 0);  
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYHOST, false);  
+        // curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);  
+        curl_setopt ( $curl, CURLOPT_HEADER, true);  
+                  
+        $response = curl_exec($curl);
+        $response = false;
+        if($response === false)  
+        {
+            // var_dump('error---' . curl_errno($curl));
+            // //CURLE_OPERATION_TIMEDOUT == 28
+            // if(in_array(curl_errno($curl), [0, 28])) 
+            curl_url($url);//超时处理操作
+            // if(curl_errno($curl) == CURLE_OPERATION_TIMEDOUT) curl_url();//超时处理操作
         }
     }
 
 
-// //初始化
-//    $curl = curl_init();
-//    //设置抓取的url
-//    curl_setopt($curl, CURLOPT_URL, 'http://127.0.0.1/index.php/Home/Socket/connectSock.html');
-//    //设置头文件的信息作为数据流输出
-//    curl_setopt($curl, CURLOPT_HEADER, 1);
-//    //设置获取的信息以文件流的形式返回，而不是直接输出。
-//    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//    //执行命令
-//    $data = curl_exec($curl);
-//    //关闭URL请求
-//    curl_close($curl);
-//    //显示获得的数据
-//    print_r($data);
+//初始化
+   // $curl = curl_init();
+   // //设置抓取的url
+   // curl_setopt($curl, CURLOPT_URL, 'http://127.0.0.1/index.php/Home/Socket/connectSock.html');
+   // //设置头文件的信息作为数据流输出
+   // curl_setopt($curl, CURLOPT_HEADER, 1);
+   // //设置获取的信息以文件流的形式返回，而不是直接输出。
+   // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   // //执行命令
+   // $data = curl_exec($curl);
+   // //关闭URL请求
+   // curl_close($curl);
+   // //显示获得的数据
+   // print_r($data);
